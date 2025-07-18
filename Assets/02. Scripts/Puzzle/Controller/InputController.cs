0d04a218cb54dd3299bab081f14eda9604eed841
Jisub Shim <jisub5322@naver.com>
2025-07-18 21:09:08 +0900

    feat: 노드 프로퍼티 생성

---
+++ Assets/02. Scripts/Puzzle/Controller/InputController.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public enum InputKey
+{
+    Up, Down, Left, Right
+}
+
+public class InputController : MonoBehaviour
+{
+    
+}
---
+++ Assets/02. Scripts/Puzzle/Controller/TileNodeSystem.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class TileNodeSystem : MonoBehaviour
+{
+    [SerializeField] private TileNode[] _tileNodes;
+    private TileNode[,] _tileNodeGrid = new TileNode[7, 7];
+    public TileNode[,] TileNodeGrid => _tileNodeGrid;
+
+    private void Awake()
+    {
+        for (int i = 0; i < 7; i++)
+        {
+            for (int j = 0; j < 7; j++)
+            {
+                _tileNodeGrid[i, j] = _tileNodes[7*i + j];
+            }
+        }
+    }
+
+    public void Action(InputKey inputKey, Vector2 manaPosition)
+    {
+
+    }
+}
