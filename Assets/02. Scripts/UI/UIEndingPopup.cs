7c56b0842c77321da5688681841388a11637a0f4
JeongJongMun <garlic@consalad.io>
2025-07-20 11:31:53 +0900

    feat : 점수 시스템 구현

--- Assets/02. Scripts/UI/UIEndingPopup.cs
+++ Assets/02. Scripts/UI/UIEndingPopup.cs
@@ -1,5 +1,6 @@
 using Core.Scripts;
 using Core.Scripts.UI;
+using DG.Tweening;
 using TMPro;
 using UnityEngine;
 
@@ -12,16 +13,24 @@ public class UIEndingPopup : UIPopup
     [SerializeField] private TextMeshProUGUI textConstructionScore;
     [SerializeField] private TextMeshProUGUI textDestructionScore;
     [SerializeField] private TextMeshProUGUI textTotalScore;
+    [SerializeField] private RectTransform transStamp;
+    [SerializeField] private RectTransform transStampOne;
+    [SerializeField] private RectTransform transStampTwo;
+    [SerializeField] private GameObject objMarkS;
+    [SerializeField] private GameObject objMarkA;
+    [SerializeField] private GameObject objMarkB;
+    [SerializeField] private GameObject objMarkC;
+    [SerializeField] private GameObject objMarkF;
     
     public override void OnAwake()
     {
         base.OnAwake();
-
         Init();
     }
     public override void Init()
     {
         base.Init();
+        buttonClose.gameObject.SetActive(false);
         
         var totalScore = GameManager.Instance.ClearTimeScore +
                          GameManager.Instance.IceCollectScore +
@@ -37,5 +46,43 @@ public class UIEndingPopup : UIPopup
         StartCoroutine(Utils.NumberCountingEffect(textConstructionScore, 0, GameManager.Instance.ConstructionScore));
         StartCoroutine(Utils.NumberCountingEffect(textDestructionScore, 0, GameManager.Instance.DestructionScore));
         StartCoroutine(Utils.NumberCountingEffect(textTotalScore, 0, totalScore));
+
+        OnStampEffect(totalScore);
+    }
+
+    private void OnStampEffect(int totalScore)
+    {
+        var sequence = DOTween.Sequence();
+        sequence.AppendInterval(2.0f);
+        sequence.Append(transStamp.DOAnchorPos(transStampOne.anchoredPosition, 0.3f).SetEase(Ease.InSine));
+        sequence.AppendCallback(() => ShowMark(totalScore) );
+        sequence.AppendInterval(0.5f);
+        sequence.Append(transStamp.DOAnchorPos(transStampTwo.anchoredPosition, 0.3f).SetEase(Ease.InSine))
+            .OnComplete(() => buttonClose.gameObject.SetActive(true));
+
+        sequence.SetLink(gameObject);
+        sequence.Play();
+    }
+
+    private void ShowMark(int totalScore)
+    {
+        switch (totalScore)
+        {
+            case >= 10000:
+                objMarkS.SetActive(true);
+                break;
+            case >= 8000:
+                objMarkA.SetActive(true);
+                break;
+            case >= 6000:
+                objMarkB.SetActive(true);
+                break;
+            case >= 3000:
+                objMarkC.SetActive(true);
+                break;
+            case < 3000:
+                objMarkF.SetActive(true);
+                break;
+        }
     }
 }
